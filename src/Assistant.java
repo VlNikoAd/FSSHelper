971ad9d28924bc3a854e5c3338ce716248666b56
Vladislav <nikolaenko_vladislav@icloud.com>
2022-07-05 15:44:10 +0300

    refactored helper.txt

--- src/Assistant.java
+++ src/Assistant.java
@@ -1,8 +1,21 @@
 import interfaces.*;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Assistant implements Health, Money, Family, Carrier, Friends {
 
-    public Assistant() {}
+    public Assistant(String helper) {
+        File file = new File(helper);
+        try {
+            if (file.createNewFile())
+                System.out.println(helper + " complete!");
+            else
+                System.out.println(helper + " something wrong!");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
     protected void sayHello() {
         System.out.println("Hello!");
--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,8 @@ public class Main {
     private static final String helper = "/Users/vladislav/helper.txt";
 
     public static void main(String[] args) {
-        createHelper(helper);
 
-        Assistant assistant = new Assistant();
+        Assistant assistant = new Assistant(helper);
         assistant.sayHello();
 
         /*
@@ -16,15 +15,5 @@ public class Main {
         2. Reader/Writer
          */
     }
-    private static void createHelper(String helper) {
-        File file = new File(helper);
-        try {
-            if (file.createNewFile())
-                System.out.println(helper + " complete!");
-            else
-                System.out.println(helper + " something wrong!");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+
 }
